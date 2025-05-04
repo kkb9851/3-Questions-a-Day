24b96a9aa6d6cf929265064264b19e49d9e335c4
KimGyeongBeom <76090919+kkb9851@users.noreply.github.com>
2025-05-04 14:44:38 +0900

    Create JadenCase 문자열 만들기.py

---
+++ 3일차/JadenCase 문자열 만들기.py
@@ -0,0 +1,23 @@
+# 오답 코드
+def solution(s):
+    words = []
+    ans = []
+    
+    s = s.lower()
+    words = s.split()
+    
+    for c in words:
+        ans.append(c.capitalize())
+    
+    return ' '.join(ans)
+
+# 2번째 풀이 코드
+def solution(s):
+    s = s.lower()
+    s = s.split(' ')
+    ans = []
+    
+    for i in s:
+        ans.append(i.capitalize())
+        
+    return ' '.join(ans)

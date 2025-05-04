460ae5494e6ced46b61f67d7ee5a218b4e0ce89b
KimGyeongBeom <76090919+kkb9851@users.noreply.github.com>
2025-05-04 14:52:10 +0900

    Update 2. JadenCase 문자열 만들기(Lv.2).py

--- 3일차/2. JadenCase 문자열 만들기(Lv.2).py
+++ 3일차/2. JadenCase 문자열 만들기(Lv.2).py
@@ -1,4 +1,5 @@
 # 1번째 코드(오답)
+# split()에서 인자를 주지 않을 경우: 연속된 공백을 하나로 처리
 def solution(s):
     words = []
     ans = []
@@ -14,6 +15,7 @@ def solution(s):
 # 2번째 풀이 코드
 def solution(s):
     s = s.lower()
+    # split(' '): 정확히 스페이스 한 칸만 구분자로 사용 
     s = s.split(' ')
     ans = []
     

5ee18d65ccf3cbc6d64df2f9b248e4b90415dc04
KimGyeongBeom <76090919+kkb9851@users.noreply.github.com>
2025-05-07 18:08:49 +0900

    Create 2. 같은 숫자는 싫어(Lv.1).py

---
+++ 6일차/2. 같은 숫자는 싫어(Lv.1).py
@@ -0,0 +1,16 @@
+# 나의 풀이
+def solution(arr):
+    answer = []
+    for i in arr:
+        if not answer or i != answer[-1]:
+            answer.append(i)
+    return answer
+
+# 다른 사람의 풀이
+def no_continuous(s):
+    # 함수를 완성하세요
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a

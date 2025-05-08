3d6a9e20c7cd15c2c3ffc49f83d851bb7cf80025
KimGyeongBeom <76090919+kkb9851@users.noreply.github.com>
2025-05-08 18:08:37 +0900

    Update 1. SWEA - 회전(D2).py

--- 7일차/1. SWEA - 회전(D2).py
+++ 7일차/1. SWEA - 회전(D2).py
@@ -13,14 +13,17 @@ for test_case in range(1, T + 1):
     print(f"#{cnt}", a[0])
     cnt += 1
 
-# 다른 사람의 풀이
-T = int(input())
+# 다른 사람의 풀이: deque 활용법
+from collections import deque
 
-for test_case in range(1, T + 1):
-    n, m = map(int, input().split())
-    queue = list(map(int, input().split()))
-    
-    for i in range(m):
-        queue.append(queue.pop(0))
-    
-    print('#{} {}'.format(test_case, queue[0]))
+for tc in range(1, int(input()) + 1):
+    N, M = map(int, input().split())
+    arr = deque(list(map(int, input().split())))
+
+    i = 0
+    while i < M:
+        back = arr.popleft()
+        arr.append(back)
+        i += 1
+
+    print('#{} {}'.format(tc, arr[0]))
